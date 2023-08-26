c43e7f31e16fd13e42707d46a2f5fc7dba481ec9
Louis Liao <buzonliao@gmail.com>
2023-08-26 19:29:19 +0800

    finish question 3

--- test/specs/test.js
+++ test/specs/test.js
@@ -37,17 +37,32 @@ describe('Cathay Bank Tests', () => {
         await $('.cubre-o-nav__menu .cubre-o-menu > div:nth-of-type(1) .cubre-o-menuLinkList__item:nth-of-type(1) .cubre-o-menuLinkList__btn .cubre-a-menuSortBtn').click()
         // card intro
         await $('.cubre-o-nav__menu .cubre-o-menu div:nth-of-type(1) .cubre-o-menuLinkList div:nth-of-type(1) .cubre-o-menuLinkList__content #lnk_Link:nth-child(2)').click()
-        // terminated card
-        await $('section:nth-of-type(6) .cubre-a-iconTitle__text').scrollIntoView()
+        
+        // const firstCardImg = await $("/html/body//article[@class='cubre-o-content']/section[6]/div[@class='cubre-o-block__wrap']//div[@class='swiper-wrapper']/div[1]//div[@class='cubre-m-compareCard__pic']/img")
 
+        // 停發卡
+        await $('a:nth-of-type(6) > p').scrollIntoView()
+        await driver.pause(3000);
+        await $('a:nth-of-type(6) > p').click()
+
+        // terminated cards
         const terminatedCards = await $$("/html/body//article[@class='cubre-o-content']/section[6]//div[@class='cubre-o-block__component']/div/div[@class='swiper-wrapper']/div")
+        
+        // scroll left on card img
+        for (let i = 0; i < terminatedCards.length; i++) {
+            await driver.pause(2000);
+
+            await driver.saveScreenshot(`./terminatedCards${i+1}.png`);
+
+            await driver.touchAction([
+                {action: 'press', x: 855, y: 1025},
+                {action: 'moveTo', x: 224, y: 1019},
+                'release'
+              ]);
+        }
+
         console.log("========");
         console.log(`There are ${terminatedCards.length} terminated credit cards`);
         console.log("========");
-        
-        // ToDo: get terminated cards' screenshot
-
-        // await driver.pause(1000);
-        // await driver.saveScreenshot('./terminatedCards.png');
     });
 })
